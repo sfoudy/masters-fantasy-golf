ef2796c85210c5756bd9d29337f8a03c0bbdbff5
sfoudy <152694248+sfoudy@users.noreply.github.com>
2025-04-11 23:24:21 +0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="masters-fantasy-golf",
+    install_requires=[
+        "streamlit-autorefresh==0.3.0",
+        "pandas==2.2.3",
+        "requests==2.32.3"
+    ]
+)
